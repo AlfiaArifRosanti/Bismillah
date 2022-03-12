b9fff97db7ce6bb5aa9c9bb647abeffefff15ef5
AlfiaArifRosanti <Alfiaarifrosanti@gmail.com>
2022-03-12 16:25:19 +0700

    menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>File Variable</title>
+</head>
+<body>
+    <h3>Nama : <?php echo "Alfia Arif Rosanti";?><h3>
+    <h3>Nim : <?php echo "200411100151";?><h3>
+<?php
+$num1 = 3;
+$num2 = 4;
+$num3 = 5;
+$hasil1=$num1 +$num2 +$num3;
+$hasil2=$num1 +$num2 -$num3;
+$hasil3=$num1 *$num2 /$num3;
+echo"Hasil Operasi Matematika yang Pertama adalah $hasil1 <br>";
+echo"Hasil Operasi Matematika yang Kedua adalah $hasil2 <br>";
+echo"Hasil Operasi Matematika yang Ketiga adalah $hasil3 <br>";
+?>
+</body>
+</html>
