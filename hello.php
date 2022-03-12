7f25b81bda5128107ffba911c3ecc68491a54729
AlfiaArifRosanti <Alfiaarifrosanti@gmail.com>
2022-03-12 16:23:15 +0700

    menambahkan file hello.php

---
+++ hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>File Hello</title>
+</head>
+<body>
+    <h1>Nama : <?php echo "Alfia Arif Rosanti";?><h1>
+    <h1>Nim : <?php echo "200411100151";?><h1>
+</body>
+</html>
